45a210b86ff483c8ffba1ea25c7709e0a688baf5
shikhantmaungs <shinkhantmaungs@gmail.com>
2023-12-21 18:57:28 +0630

    feat: updated endpoint url

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,3 @@
-export const endpoint = 'http://localhost:3000/api/'
+export const endpoint = 'https://incompatible-food.vercel.app/'
 export const source_link = 'https://github.com/sannlynnhtun-coding/IncompatibleFood/'
 export const contribute_link = ''
